ac768a290a3ea618edcd302148712d859c507942
Alexander Baranov <elfanlide@gmail.com>
2016-12-08 18:10:08 +0300

    + convert main page (html+styles)

--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -13,7 +13,15 @@ require('./bootstrap');
  * the application, or feel free to tweak this setup for your needs.
  */
 
-Vue.component('example', require('./components/Example.vue'));
+Vue.component('maf-navbar', require('./components/maf-navbar.vue'));
+Vue.component('maf-our-customers-section', require('./components/maf-our-customers-section.vue'));
+Vue.component('maf-blog-latest-section', require('./components/maf-blog-latest-section.vue'));
+Vue.component('maf-partners-section', require('./components/maf-partners-section.vue'));
+Vue.component('maf-footer', require('./components/maf-footer.vue'));
+Vue.component('maf-app', require('./components/index.vue'));
+
+Vue.component('maf-location-form-base', require('./components/maf-location-form-base.vue'));
+Vue.component('maf-location-form', require('./components/maf-location-form.vue'));
 
 const app = new Vue({
     el: '#app'
---
+++ resources/assets/js/components/maf-navbar.vue
@@ -0,0 +1,18 @@
+<template>
+    <nav class="navbar navbar-default">
+        <div class="container-fluid">
+            <div class="navbar-header">
+                <a class="maf-logo navbar-brand" href="/"></a>
+            </div>
+
+            <div class="navbar-right">
+                <div class='contact iveal'>
+                    <span class="i phone16"></span>
+                    <a href="tel:+18559083506">
+                        (855) 908-3506
+                    </a>
+                </div>
+            </div>
+        </div>
+    </nav>
+</template>
