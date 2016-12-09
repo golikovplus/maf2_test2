e5161b216257a97e4269ee222c39b6aa021c509d
Yuriy Golikov <golikov.yura@ritarsoft.com>
2016-12-09 15:33:21 +0200

    jsUnitTest

---
+++ resources/assets/js/components/alert.vue
@@ -0,0 +1,37 @@
+<style>
+    .alert {
+        padding: 2em;
+    }
+    .alert-Success {
+        border: 10px solid green;
+    }
+    .alert-Error {
+        border: 10px solid red;
+    }
+</style>
+
+<template>
+    <div class="alert"
+         v-bind:class="{
+            'alert-Success': type == 'success',
+            'alert-Error': type == 'error',
+        }"
+         v-show="show">
+        <p>
+            {{ msg }}
+        </p>
+    </div>
+</template>
+
+<script>
+    export default {
+        data() {
+            return {
+                show: true
+            }
+        },
+        props: ['msg', 'type'],
+                ready() {
+        }
+    }
+</script>
