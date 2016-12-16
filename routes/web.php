52b85a6e57436200a6ab4863a7971ddab6a4bfa5
Yuriy Golikov <golikov.yura@ritarsoft.com>
2016-12-16 15:24:56 +0200

    more

--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,20 @@
 |
 */
 
-Route::get('/', function () {
-    return view('index');
+Route::group(['middleware' => 'web'], function () {
+
+    Route::get('/', function () {return view('index');})->name('/');
+    Route::get('/backend', function () {return view('backend');})->name('backend');
+
+    Route::any('/location/{location}', function () {return view('index');});
+
+    Route::get('/api/lead/{id?}', 'Leads@index')->name('lead.index');
+    Route::post('/api/lead', 'Leads@store')->name('lead.store');
+    Route::post('/api/lead/{id}', 'Leads@update')->name('lead.update');
+    Route::delete('/api/lead/{id}', 'Leads@destroy')->name('lead.destroy');
+
+    Route::get('/api/testimonial/{id?}', 'Testimonials@index')->name('testimonial.index');
+    Route::post('/api/testimonial', 'Testimonials@store')->name('testimonial.store');
+    Route::post('/api/testimonial/{id}', 'Testimonials@update')->name('testimonial.update');
+    Route::delete('/api/testimonial/{id}', 'Testimonials@destroy')->name('testimonial.destroy');
 });
