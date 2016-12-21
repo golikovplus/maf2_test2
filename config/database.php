d2e5afe85e3d3f9bb305aae14345865ae5975627
Yuriy Golikov <golikov.yura@ritarsoft.com>
2016-12-21 13:57:17 +0200

    her2

--- config/database.php
+++ config/database.php
@@ -1,14 +1,6 @@
 <?php
 
-if (getenv('DATABASE_URL')) {
-    $url = parse_url(getenv('DATABASE_URL'));
 
-    putenv('DB_HOST='.$url['host']);
-    putenv('DB_USERNAME='.$url['user']);
-    putenv('DB_PORT='.$url['port']);
-    putenv('DB_PASSWORD='.$url['pass']);
-    putenv('DB_DATABASE='.ltrim($url['path'], '/'));
-}
 
 return [
 
