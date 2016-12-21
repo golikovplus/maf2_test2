9c09105f5470e14dd0f49bc736ad88a3e01cb752
Alexander Baranov <elfanlide@gmail.com>
2016-12-21 18:14:26 +0300

    + styles for mobile finished

--- resources/assets/js/components/form/contact-info.vue
+++ resources/assets/js/components/form/contact-info.vue
@@ -1,25 +1,20 @@
 <template xmlns:v-bind="http://www.w3.org/1999/xhtml">
     <div>
         <div class="row">
-            <div class="col-md-10 col-md-offset-1 col-ms-12 col-xs-12 col-ms-offset-0 col-xs-offset-0 teal_ce">
+            <div class="col-lg-10 col-lg-offset-1 col-md-12 col-ms-12 col-xs-12 col-ms-offset-0 col-xs-offset-0 teal_ce">
                 <h1>Nice Work!</h1>
 
                 <h3>Please fill out the information below so our Agent<br />
                     Advisor can reach out to you within 24 hours to.</h3>
 
                 <div class="row items">
-                    <div class="hidden-md hidden-lg center-block">
-                        <span class="item-circle">1</span> Review your results
-                        <span class="item-circle">2</span> Answer any questions
-                        <span class="item-circle">3</span> Connect you with a qualified agent
-                    </div>
-                    <div class="small-col-padding col-md-3 col-md-offset-1 hidden-sm hidden-xs">
+                    <div class="small-col-padding col-md-3 col-md-offset-0 col-lg-offset-0 col-sm-6 col-sm-offset-3 col-xs-12 col-xs-offset-0 col-md-offset-1">
                         <span class="item-circle">1</span> Review your results
                     </div>
-                    <div class="small-col-padding col-md-3 col-md-offset-0 hidden-sm hidden-xs">
+                    <div class="small-col-padding col-md-3 col-md-offset-0 col-lg-offset-1 col-sm-6 col-sm-offset-3 col-xs-12 col-xs-offset-0">
                         <span class="item-circle">2</span> Answer any questions
                     </div>
-                    <div class="small-col-padding col-md-4 col-md-offset-0 hidden-sm hidden-xs">
+                    <div class="small-col-padding col-md-4 col-md-offset-0 col-lg-offset-1 col-sm-6 col-sm-offset-3 col-xs-12 col-xs-offset-0">
                         <span class="item-circle">3</span> Connect you with a qualified agent
                     </div>
                 </div>
