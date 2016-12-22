90ff24b8887ac66c2226634bb3c9371be786b8e0
Yuriy Golikov <golikov.yura@ritarsoft.com>
2016-12-22 17:10:07 +0200

    del

--- pull_from_remote.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-git remote set-url origin git@github.com:Agentology/maf-2-0.git
-git checkout develop
-git pull
-git remote set-url origin git@github.com:golikovplus/maf2_test2.git
-git remote -v
-git push origin master
